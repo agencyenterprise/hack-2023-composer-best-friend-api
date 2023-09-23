fa2cf3db0bfa4957826c37b84618d415e1d35e29
Bruno <brunovieirapoeta@gmail.com>
2023-09-23 09:53:15 -0300

    Search feature (OpenAI generation)

--- dist/routes/user-routes.js
+++ dist/routes/user-routes.js
@@ -11,10 +11,15 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 const express_1 = require("express");
+const clerk_sdk_node_1 = require("@clerk/clerk-sdk-node");
 const user_controller_1 = require("../controllers/user-controller");
 exports.router = (0, express_1.Router)();
 exports.router.post('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     yield (0, user_controller_1.create)(req.body);
+    return res.status(200).send();
+}));
+exports.router.post('/key', (0, clerk_sdk_node_1.ClerkExpressRequireAuth)({}), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    yield (0, user_controller_1.updateKey)(req.auth.userId, req.body.key);
     return res.status(200).send();
 }));
 //# sourceMappingURL=user-routes.js.map
---
+++ src/routes/search-mocks.ts
@@ -0,0 +1,163 @@
+export default {
+    "variations": [
+        {
+            "notes": [
+                {
+                    "pitch": [
+                        "D4",
+                        "F#4",
+                        "A4"
+                    ],
+                    "duration": 1,
+                    "velocity": 70
+                },
+                {
+                    "pitch": [
+                        "B3",
+                        "D4",
+                        "F#4"
+                    ],
+                    "duration": 1,
+                    "velocity": 75
+                },
+                {
+                    "pitch": [
+                        "G3",
+                        "B3",
+                        "D4"
+                    ],
+                    "duration": 1,
+                    "velocity": 80
+                },
+                {
+                    "pitch": [
+                        "E4",
+                        "G#4",
+                        "B4"
+                    ],
+                    "duration": 1,
+                    "velocity": 70
+                }
+            ]
+        },
+        {
+            "notes": [
+                {
+                    "pitch": [
+                        "C4",
+                        "E4",
+                        "G4"
+                    ],
+                    "duration": 2,
+                    "velocity": 75
+                },
+                {
+                    "pitch": [],
+                    "duration": 2
+                },
+                {
+                    "pitch": [
+                        "D4",
+                        "F#4",
+                        "A4"
+                    ],
+                    "duration": 1,
+                    "velocity": 70
+                },
+                {
+                    "pitch": [
+                        "A3",
+                        "C#4",
+                        "E4"
+                    ],
+                    "duration": 1,
+                    "velocity": 80
+                }
+            ]
+        },
+        {
+            "notes": [
+                {
+                    "pitch": [
+                        "B3",
+                        "D#4",
+                        "F#4"
+                    ],
+                    "duration": 1,
+                    "velocity": 80
+                },
+                {
+                    "pitch": [
+                        "E4",
+                        "G#4",
+                        "B4"
+                    ],
+                    "duration": 1,
+                    "velocity": 75
+                },
+                {
+                    "pitch": [
+                        "F#4",
+                        "A#4",
+                        "C#5"
+                    ],
+                    "duration": 1,
+                    "velocity": 70
+                },
+                {
+                    "pitch": [
+                        "D#3",
+                        "F#3",
+                        "A#3"
+                    ],
+                    "duration": 1,
+                    "velocity": 75
+                }
+            ]
+        },
+        {
+            "notes": [
+                {
+                    "pitch": [
+                        "A3",
+                        "C#4",
+                        "E4"
+                    ],
+                    "duration": 1,
+                    "velocity": 70
+                },
+                {
+                    "pitch": [
+                        "F#3",
+                        "A3",
+                        "C#4"
+                    ],
+                    "duration": 1,
+                    "velocity": 80
+                },
+                {
+                    "pitch": [
+                        "D4",
+                        "F#4",
+                        "A4"
+                    ],
+                    "duration": 2,
+                    "velocity": 70
+                },
+                {
+                    "pitch": [],
+                    "duration": 2
+                },
+                {
+                    "pitch": [
+                        "B3",
+                        "D#4",
+                        "F#4"
+                    ],
+                    "duration": 1,
+                    "velocity": 75
+                }
+            ]
+        }
+    ]
+}

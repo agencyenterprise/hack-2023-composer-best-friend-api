5e142dc8fb58c76b27093541c638d465a26ab303
leandrovi <leandrofernandesvieira@gmail.com>
2023-09-26 07:00:21 -0300

    fix: fix user creation endpoint

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,31 +1,39 @@
-import * as mongoose from 'mongoose';
+import * as mongoose from "mongoose";
 
-import { UserSchema } from '../models/user';
+import { UserSchema } from "../models/user";
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 
-export function create(user: any){
+async function createUser(user: any) {
   const newUser = new User(user);
   return newUser.save();
 }
 
-export async function countUsage(userId: string) {
-  let user = await User.findOne({ clerkUserId: userId }).exec();
-  if(!user){
-    await create({ clerkUserId: userId, usageCount: 1 })
-  } else {
-    user.usageCount = user.usageCount + 1
-    await user.save()
+export async function create(clerkUserId: string) {
+  let user = await User.findOne({ clerkUserId: clerkUserId }).exec();
+  if (!user) {
+    await createUser({ clerkUserId: clerkUserId, usageCount: 0 });
+    return user.save();
   }
-  
+  return user;
 }
 
-export function getUserByClerckId(clerkUserId: string){
+export async function countUsage(userId: string) {
+  let user = await User.findOne({ clerkUserId: userId }).exec();
+  if (!user) {
+    await createUser({ clerkUserId: userId, usageCount: 1 });
+  } else {
+    user.usageCount = user.usageCount + 1;
+    await user.save();
+  }
+}
+
+export function getUserByClerckId(clerkUserId: string) {
   return User.findOne({ clerkUserId: clerkUserId }).exec();
 }
 
-export async function updateKey(userId: string, key: string){
+export async function updateKey(userId: string, key: string) {
   const user = await User.findOne({ clerkUserId: userId }).exec();
-  user.key = key
-  await user.save()
+  user.key = key;
+  await user.save();
 }
